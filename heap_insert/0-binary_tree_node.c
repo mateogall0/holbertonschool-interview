9fb51ead550f9681f037da7db455c8d2a7e51648
gallit0 <mateogesede@gmail.com>
2023-04-05 18:05:49 -0300

    hot fix

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -10,10 +10,12 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new = malloc(sizeof(binary_tree_t));
-    if (!new)
-        return NULL;
-    new->parent = parent;
-    new->n = value;
-    return new;
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return NULL;
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	return new;
 }
