e302aa0686e6af001ea4d9659ec12b4d6232fac9
mateogall0 <mateogesede@gmail.com>
2023-05-12 10:44:10 -0300

    done

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+
+int check_cycle(listint_t *list)
+{
+	listint_t *p = NULL;
+
+	if (!list)
+		return (0);
+
+	for (p = list->next; p && p->next; p = p->next)
+		if (p == list)
+			return (1);
+
+	return (0);
+}
