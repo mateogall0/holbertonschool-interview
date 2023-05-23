8332b5fc605b42d15bf0e20fc92789a073d9a898
mateogall0 <mateogesede@gmail.com>
2023-05-23 13:27:28 -0300

    done

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
+"""
+UTF-8
+"""
 
 
 def validUTF8(data):
     i = 0
     while i < len(data):
         num = data[i]
-        if num < 128:  # ASCII character
+        if num < 128:
             i += 1
-        elif 128 <= num <= 191:  # Continuation byte
+        elif 128 <= num <= 191:
             return False
-        else:  # Multi-byte sequence
-            # Determine the number of bytes in the sequence based on the leading ones
-            if num < 224:  # Two-byte sequence
+        else:
+            if num < 224:
                 expected_bytes = 2
-            elif num < 240:  # Three-byte sequence
+            elif num < 240:
                 expected_bytes = 3
-            elif num < 248:  # Four-byte sequence
+            elif num < 248:
                 expected_bytes = 4
             else:
                 return False
-            
-            # Check if the following bytes are continuation bytes
+
             for j in range(1, expected_bytes):
                 i += 1
                 if i >= len(data) or not (128 <= data[i] <= 191):
                     return False
         i += 1
-    
+
     return True
