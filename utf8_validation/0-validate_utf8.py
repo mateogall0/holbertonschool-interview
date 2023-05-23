b5e036f7f647b2761822a6a204f446a79ea16e78
mateogall0 <mateogesede@gmail.com>
2023-05-23 13:33:58 -0300

    done

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -9,13 +9,15 @@ def validUTF8(data):
 
     for byte in data:
         if num_bytes == 0:
-            if (byte >> 5) == 0b110:
+            if (byte >> 7) == 0:
+                num_bytes = 0
+            elif (byte >> 5) == 0b110:
                 num_bytes = 1
             elif (byte >> 4) == 0b1110:
                 num_bytes = 2
             elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            elif (byte >> 7) != 0:
+            else:
                 return False
         else:
             if (byte >> 6) != 0b10:
